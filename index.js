ab15d58d2fdc397e304c2efd6b7f92ef31d2b402
Flint Rose <oliverflintrose@gmail.com>
2025-04-03 11:51:02 -0500

    fixed the express instance

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
-app.use(express.static("pics"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
 var server = app.listen(8000, function () {
   var host = server.address().address;
