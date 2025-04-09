c48656e98a36dffa64aaaede54ff43e82075cda6
Flint Rose <oliverflintrose@gmail.com>
2025-04-08 20:49:38 -0500

    worked on server a bit:

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/status", (request, response) => {
+  const status = {
+    Status: "running",
+  };
+  response.send(status);
+});
+
 var server = app.listen(3030, function () {
   var host = "localhost";
   var port = server.address().port;
